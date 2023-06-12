343f76b56ff35c3421314e5adf3178fb5ffd2d4f
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-12 18:43:45 +0300

    Metadata for [ id ] Для динамических роутов Metadata прописывется в виде функции. Кстати, измененные данные в Metadata видно только после обновления страницы

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,6 +4,12 @@ type Props = {
 	}
 }
 
+export function generateMetadata({ params: { id } }: Props) {
+	return {
+		title: id
+	}
+}
+
 export default function Post({ params: { id } }: Props) {
 	return <h1>Post page {id}</h1>
 }
