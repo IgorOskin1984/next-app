714503ef3a94ebd3ae7f30dd091d43ddba13cae3
IgorOskin1984 <91606306+IgorOskin1984@users.noreply.github.com>
2023-06-12 18:28:07 +0300

    AboutLayout в папке About создал layout.tsx, где отрисовал необходимые эл-ты

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,3 @@
 export default function About() {
-	return <h1>About page</h1>
+	return <h4>About page</h4>
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 
 export default function Home() {
 	return <h1>Welcome to NextJS world</h1>
